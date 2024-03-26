872dcd545751ab88d53c464d0b5fa861469b95b6
George Tsitsiridis <gtsitsiridis@gmail.com>
2024-03-26 13:50:09 +0100

    tests to verify that the sequences are correct

--- tests/test_veff_dataloader.py
+++ tests/test_veff_dataloader.py
@@ -22,12 +22,40 @@ def chr22_example_files():
 @pytest.fixture
 def variants():
     return {
-        'chr22:17702426:C>T': {
+        'chr22:16364873:G>A_ENST00000438441.1': {
             'chrom': 'chr22',
-            'start': 17702425,
-            'end': 17702426,
-            'ref': 'C',
-            'alt': 'T',
+            'ref_start': 16364856,
+            'ref_end': 16364877,
+            'start': 16364872,
+            'end': 16364873,
+            'ref': 'G',
+            'alt': 'A',
+            'ref_seq': 'ACTGGCTGGCCATGCCGTCCC',
+            'alt_seq': 'ACTGGCTGGCCATGCCATCCC',
+        },
+        'chr22:19420453:A>ATATT_ENST00000471259.1_1': {
+            'chrom': 'chr22',
+            'ref_start': 19420451,
+            'ref_end': 19420472,
+            'start': 19420452,
+            'end': 19420453,
+            'ref': 'A',
+            'alt': 'ATATT',
+            'ref_seq': 'GATATTTATTTATTTATTTGA',
+            'alt_seq': 'TTTATTTATTTATTTATTTGA',
+        },
+        'chr22:18359465:GTTATGGAGGTTAGGGAGGTTATGGAGGTTAGGGAGC>G_ENST00000462645.1_3': {
+            'chrom': 'chr22',
+            'ref_start': 18359458,
+            'ref_end': 18359479,
+            'start': 18359464,
+            'end': 18359501,
+            'ref': 'GTTATGGAGGTTAGGGAGGTTATGGAGGTTAGGGAGC',
+            'alt': 'G',
+            # complement 'TGCAGGGTTATGGAGGTTAGG',
+            'ref_seq': 'CCTAACCTCCATAACCCTGCA',
+            # complement 'CACATGCAGGGTTATGGAGGT',
+            'alt_seq': 'ACCTCCATAACCCTGCATGTG',
         },
     }
 
@@ -72,13 +100,25 @@ def test_get_roi_from_genome_annotation(chr22_example_files):
     assert roi_i.End == (11066501 + downstream_tss)
 
 
-def test_dataloader(chr22_example_files):
+def test_dataloader(chr22_example_files, variants):
     dl = VCF_Enformer_DL(
         fasta_file=chr22_example_files['fasta'],
         gtf_file=chr22_example_files['gtf'],
         vcf_file=chr22_example_files['vcf'],
+        is_onehot=False
     )
     total = 0
+    checked_variants = set()
     for i in dl:
         total += 1
+        var_id = f'{i["metadata"]["variant"]["str"]}_{i["metadata"]["transcript_id"]}'
+        variant = variants.get(var_id, None)
+        if variant is not None:
+            checked_variants.add(var_id)
+            # check alt sequence
+            assert i['inputs']['ref_seq'] == variant['ref_seq']
+            assert i['inputs']['alt_seq'] == variant['alt_seq']
         print(i)
+
+    # check that all variants in my list were found and checked
+    assert checked_variants == set(variants.keys())
