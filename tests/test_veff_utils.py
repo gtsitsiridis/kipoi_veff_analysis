649d32cb96369861c2e51d52bd94f0d275f718f0
George Tsitsiridis <gtsitsiridis@gmail.com>
2024-04-02 16:12:26 +0200

    Enformer to parquet

--- tests/test_veff_utils.py
+++ tests/test_veff_utils.py
@@ -29,14 +29,14 @@ def random_model():
     return RandomModel()
 
 
-def test_random_enformer(chr22_example_files, random_model):
+def run_enformer(example_files, model):
     batch_size = 3
     size = 10
 
     dl = VCF_Enformer_DL(
-        fasta_file=chr22_example_files['fasta'],
-        gtf_file=chr22_example_files['gtf'],
-        vcf_file=chr22_example_files['vcf'],
+        fasta_file=example_files['fasta'],
+        gtf_file=example_files['gtf'],
+        vcf_file=example_files['vcf'],
         is_onehot=True,
         downstream_tss=500,
         upstream_tss=500,
@@ -45,10 +45,14 @@ def test_random_enformer(chr22_example_files, random_model):
         size=size
     )
 
-    model = Enformer(model=random_model)
-    results = model.predict(dl, batch_size=batch_size)
+    enformer = Enformer(model=model)
+    results = enformer.predict(dl, batch_size=batch_size)
     assert len(results) == size
 
     output_dir = Path('output/test')
     output_dir.mkdir(exist_ok=True, parents=True)
     Enformer.to_parquet(results, output_dir / 'random_enformer.parquet')
+
+
+def test_random_enformer(chr22_example_files, random_model):
+    run_enformer(chr22_example_files, random_model)
