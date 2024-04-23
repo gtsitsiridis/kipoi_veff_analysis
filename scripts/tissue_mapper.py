867ffd6cb2ceb46d53ffe461dc89a3651d317de3
George Tsitsiridis <gtsitsiridis@gmail.com>
2024-04-23 16:54:47 +0200

    added snakemake pipeline

---
+++ scripts/tissue_mapper.py
@@ -0,0 +1,15 @@
+from kipoi_enformer.enformer import EnformerTissueMapper
+from kipoi_enformer.logger import setup_logger
+from pathlib import Path
+
+logger = setup_logger()
+
+# SNAKEMAKE SCRIPT
+config = snakemake.config
+input_ = snakemake.input
+output = snakemake.output
+wildcards = snakemake.wildcards
+
+tissue_mapper = EnformerTissueMapper(tracks_path=input_['tracks_path'],
+                                     tissue_matcher_path=input_['tissue_matcher_path'])
+tissue_mapper.predict(input_['enformer_dir'], output_path=output['prediction_dir'])
