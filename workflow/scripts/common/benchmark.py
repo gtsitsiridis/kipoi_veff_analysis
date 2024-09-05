79cbacb1662ef22ac2135c2b84f8a6930a646bf3
George Tsitsiridis <gtsitsiridis@gmail.com>
2024-09-05 15:32:12 +0200

    added non-tissue specific benchmark

--- workflow/scripts/common/benchmark.py
+++ workflow/scripts/common/benchmark.py
@@ -11,73 +11,75 @@ config = snakemake.config
 
 logger = logging.getLogger()
 
+# variables
+tissues = config['benchmark']['tissues']
+fdr_cutoff = config['benchmark']['fdr_cutoff']
+annotation_df = pl.scan_parquet(config['benchmark']['annotation_path']).unique().rename({'gene': 'gene_id'})
+folds_df = pl.scan_parquet(config['benchmark']['folds_path'])
+genotypes_df = pl.scan_parquet(config['benchmark']['genotypes_path'], hive_partitioning=True).select(
+        ['sampleId', 'chrom', 'start', 'end', 'ref', 'alt']).rename(
+        {'sampleId': 'individual',
+         'start': 'variant_start',
+         'end': 'variant_end', })
+variant_effects_paths = list(input_['veff_path'])
+output_path = output['benchmark_path']
 
-class VeffBenchmark:
+# benchmarking
+logger.info('Benchmarking...')
+# load gene specific variant effect data
+gene_veff_df = pl.concat([pl.scan_parquet(path) for path in variant_effects_paths]).select(
+    pl.col(['tissue', 'gene_id']),
+    pl.col('strand').cast(pl.Enum(['-', '+'])),
+    pl.col(['chrom', 'variant_start', 'variant_end', 'ref', 'alt', 'veff_score'])
+).with_columns(
+    pl.col('gene_id').str.replace(r'([^\.]+)\..+$', "${1}").alias('gene_id'))
 
-    def __init__(self, annotation_path: str | Path, genotypes_path: str | Path,
-                 folds_path: str | Path, fdr_cutoff: float = 0.2):
-        self.fdr_cutoff = fdr_cutoff
-        self.annotation_df = pl.scan_parquet(annotation_path).unique().rename({'gene': 'gene_id'})
-        self.folds_df = pl.scan_parquet(folds_path)
-        self.genotypes_df = pl.scan_parquet(genotypes_path, hive_partitioning=True).select(
-            ['sampleId', 'chrom', 'start', 'end', 'ref', 'alt']).rename(
-            {'sampleId': 'individual',
-             'start': 'variant_start',
-             'end': 'variant_end', })
+# split gene_veff to tissue specific and non-tissue specific
+tissue_gene_veff_df = gene_veff_df.filter(pl.col('tissue').is_not_null()). \
+    filter(pl.col('tissue').is_in(tissues))
 
-    def run(self, variant_effects_paths: list[str | Path], output_path: str | Path):
-        logger.info('Benchmarking...')
-        gene_veff_df = pl.concat([pl.scan_parquet(path) for path in variant_effects_paths]).select(
-            pl.col(['tissue', 'gene_id']),
-            pl.col('strand').cast(pl.Enum(['-', '+'])),
-            pl.col(['chrom', 'variant_start', 'variant_end', 'ref', 'alt', 'veff_score'])
-        ).with_columns(
-            pl.col('gene_id').str.replace(r'([^\.]+)\..+$', "${1}").alias('gene_id'))
+# populate non-tissue specific gene_veff with all tissues
+non_tissue_gene_veff_df = gene_veff_df.filter(pl.col('tissue').is_null()). \
+    select(pl.exclude('tissue')). \
+    join(pl.DataFrame({'tissue': tissues}).lazy(), on=None, how='cross')
 
-        # todo assert that the index is unique
+# concatanate tissue specific and non-tissue specific gene_veff
+gene_veff_df = pl.concat([tissue_gene_veff_df, non_tissue_gene_veff_df])
 
-        veff_df = self.genotypes_df.join(
-            gene_veff_df, on=['chrom', 'variant_start', 'variant_end', 'ref', 'alt'], how='inner'
-        ).with_columns(
-            (pl.col('veff_score').abs() == pl.col('veff_score').abs().max())
-            .over(['gene_id', 'tissue', 'individual']).alias('top')
-        ).filter(pl.col('top')).group_by(['gene_id', 'tissue', 'individual']).agg(
-            pl.exclude('top').first()
+# todo assert that the index is unique
+
+# join genotypes with variant effect data
+veff_df = genotypes_df.join(
+    gene_veff_df, on=['chrom', 'variant_start', 'variant_end', 'ref', 'alt'], how='inner'
+).with_columns(
+    (pl.col('veff_score').abs() == pl.col('veff_score').abs().max())
+    .over(['gene_id', 'tissue', 'individual']).alias('top')
+).filter(pl.col('top')).group_by(['gene_id', 'tissue', 'individual']).agg(
+    pl.exclude('top').first()
+)
+
+logger.info('Joining annotation and variant effect tables...')
+
+# join genome annotatino with folds and variant effect data
+annotation_folds_df = annotation_df.join(folds_df, on='individual', how='inner')
+res_df = annotation_folds_df.join(veff_df, on=['individual', 'gene_id', 'tissue'], how='left')
+# fill null with 0 for veff_score
+res_df = res_df.with_columns(pl.col('veff_score').fill_null(0))
+res_df = res_df.with_columns(
+    (
+        pl.when(pl.col('FDR') > fdr_cutoff)
+        .then(pl.lit('normal'))
+        .otherwise(
+            pl.when(pl.col('zscore') > 0)
+            .then(pl.lit('overexpressed'))
+            .otherwise(
+                pl.when(pl.col('zscore') < 0)
+                .then(pl.lit('underexpressed'))
+                # this should never be the case
+                .otherwise(pl.lit('CHECK'))
+            )
         )
-
-        # logger.info('Checking whether the variant effect table is empty...')
-        # if veff_df.collect().is_empty():
-        #     logger.warning('The variant effect table is empty... All records will be predicted as normal.')
-
-        logger.info('Joining annotation and variant effect tables...')
-
-        annotation_folds_df = self.annotation_df.join(self.folds_df, on='individual', how='inner')
-        res_df = annotation_folds_df.join(veff_df, on=['individual', 'gene_id', 'tissue'], how='left')
-        # fill null with 0 for veff_score
-        res_df = res_df.with_columns(pl.col('veff_score').fill_null(0))
-        res_df = res_df.with_columns(
-            (
-                pl.when(pl.col('FDR') > self.fdr_cutoff)
-                .then(pl.lit('normal'))
-                .otherwise(
-                    pl.when(pl.col('zscore') > 0)
-                    .then(pl.lit('overexpressed'))
-                    .otherwise(
-                        pl.when(pl.col('zscore') < 0)
-                        .then(pl.lit('underexpressed'))
-                        # this should never be the case
-                        .otherwise(pl.lit('CHECK'))
-                    )
-                )
-            ).cast(pl.Enum(['underexpressed', 'normal', 'overexpressed'])).alias('outlier_state')
-        )
-        logger.info('Collecting results...')
-        res_df.collect().write_parquet(output_path)
-
-
-benchmark = VeffBenchmark(annotation_path=config['benchmark']['annotation_path'],
-                          genotypes_path=config['benchmark']['genotypes_path'],
-                          folds_path=config['benchmark']['folds_path'],
-                          fdr_cutoff=config['benchmark']['fdr_cutoff'])
-
-benchmark.run(list(input_['veff_path']), output_path=output['benchmark_path'])
+    ).cast(pl.Enum(['underexpressed', 'normal', 'overexpressed'])).alias('outlier_state')
+)
+logger.info('Collecting results...')
+res_df.collect().write_parquet(output_path)
