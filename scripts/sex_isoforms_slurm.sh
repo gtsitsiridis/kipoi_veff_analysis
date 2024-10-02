a762f292853467b96922880043ea7bac102272e3
George Tsitsiridis <gtsitsiridis@gmail.com>
2024-10-03 01:47:07 +0200

    added sex_isoforms test

--- scripts/sex_isoforms_slurm.sh
+++ scripts/sex_isoforms_slurm.sh
@@ -6,20 +6,14 @@ GTEX_ANNOTATION_PATH="/s/project/gtex_genetic_diagnosis/v8/sample_annotation.tsv
 GTF_PATH="/s/project/rep/processed/training_results_v15/gtex_v8_old_dna/gtf_transcripts.parquet"
 GTEX_TRANSCRIPT_TPM_PATH="/s/project/rep/processed/training_results_v15/general/gtex_transcript_tpms.zarr/"
 OUTPUT_PATH="/s/project/promoter_prediction/sex_analysis/isoform_proportions"
-
-# Read the number of genes
-#NUM_GENES=$(wc -l < "$GENES_PATH")
-NUM_GENES=4
+LOG_PATH='logs'
 
 # Create log directory if it doesn't exist
-mkdir -p logs
+mkdir -p $LOG_PATH
 
-# Submit a job array
-sbatch --array=0-$(($NUM_GENES-1)) --ntasks=200 --partition=lowprio<< EOF
-#!/bin/bash
 #SBATCH --job-name=sex_isoforms
-#SBATCH --output=logs/sex_isoforms_%A_%a.out
-#SBATCH --error=logs/sex_isoforms_%A_%a.err
+#SBATCH --output=${LOG_PATH}/sex_isoforms_%A_%a.out
+#SBATCH --error=${LOG_PATH}/sex_isoforms_%A_%a.err
 #SBATCH --time=01:00:00
 #SBATCH --mem=4G
 
@@ -27,5 +21,6 @@ echo "Slurm Job Id SLURM_ARRAY_JOB_ID is ${SLURM_ARRAY_JOB_ID}"
 echo "Slurm job array index SLURM_ARRAY_TASK_ID value is ${SLURM_ARRAY_TASK_ID}"
 
 # Run the Python script with the gene index
-python scripts/sex_isoforms.py --gtex_annotation_path ${GTEX_ANNOTATION_PATH} --gtf_path ${GTF_PATH} --gtex_transcript_tpm_path ${GTEX_TRANSCRIPT_TPM_PATH} --genes_path ${GENES_PATH} --output_path ${OUTPUT_PATH} --gene_index $SLURM_ARRAY_TASK_ID
-EOF
+python scripts/sex_isoforms.py --gtex_annotation_path ${GTEX_ANNOTATION_PATH} \
+  --gtf_path ${GTF_PATH} --gtex_transcript_tpm_path ${GTEX_TRANSCRIPT_TPM_PATH} \
+  --genes_path ${GENES_PATH} --output_path ${OUTPUT_PATH} --gene_index $SLURM_ARRAY_TASK_ID
